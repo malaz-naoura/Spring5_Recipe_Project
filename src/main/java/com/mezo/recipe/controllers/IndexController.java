2d849cb95881d4942f8a8917cda157c85eaa6d31
malaz_naoura <malaznaoura@gmail.com>
2023-12-13 11:43:48 +0300

    Create index Page and index controller

---
+++ src/main/java/com/mezo/recipe/controllers/IndexController.java
@@ -0,0 +1,14 @@
+package com.mezo.recipe.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"","/","/index","/index/"})
+    String getIndexPage(){
+        return "index";
+    }
+
+}
