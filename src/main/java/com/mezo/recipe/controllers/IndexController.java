e2f17a524d07bbaca06c4aacfc510e9ed69b52d8
malaz_naoura_dell <malaznaoura@gmail.com>
2023-12-27 10:15:30 +0300

    Remove unnecessary code

--- src/main/java/com/mezo/recipe/controllers/IndexController.java
+++ src/main/java/com/mezo/recipe/controllers/IndexController.java
@@ -1,9 +1,5 @@
 package com.mezo.recipe.controllers;
 
-import com.mezo.recipe.domains.Category;
-import com.mezo.recipe.domains.UnitOfMeasure;
-import com.mezo.recipe.repositories.CategoryRepo;
-import com.mezo.recipe.repositories.UnitOfMeasureRepo;
 import com.mezo.recipe.services.RecipeServiceImpl;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -11,30 +7,20 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
-import java.util.Optional;
 
 @Slf4j
 @Controller
 public class IndexController {
 
-    CategoryRepo categoryRepo;
-    UnitOfMeasureRepo unitOfMeasureRepo;
     RecipeServiceImpl recipeServiceImpl;
 
-    public IndexController(CategoryRepo categoryRepo, UnitOfMeasureRepo unitOfMeasureRepo, RecipeServiceImpl recipeServiceImpl) {
-        this.categoryRepo = categoryRepo;
-        this.unitOfMeasureRepo = unitOfMeasureRepo;
+    public IndexController(RecipeServiceImpl recipeServiceImpl) {
         this.recipeServiceImpl = recipeServiceImpl;
     }
 
     @RequestMapping({"", "/", "/index", "/index/"})
     String getIndexPage(Model model) {
         log.debug("Index page is loaded");
-        Optional<Category> categoryOptional = categoryRepo.findByDescription("American");
-        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepo.findByDescription("Cup");
-
-//        categoryOptional.ifPresent(category -> System.out.println(category.getId()));
-//        unitOfMeasureOptional.ifPresent(unitOfMeasure -> System.out.println(unitOfMeasure.getId()));
 
         ArrayList recipes = recipeServiceImpl.getAllRecipes();
         model.addAttribute("recipes",recipes);
